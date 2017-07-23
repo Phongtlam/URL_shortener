235419e2e8f0e63ca6d04cea329e48079f24ec21
Phongtlam <Phongtlam@gmail.com>
2017-07-22 20:07:56 -0700

    heroku

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '..', 'public')));
+  app.use(express.static(path.resolve(__dirname, '..', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
   });
